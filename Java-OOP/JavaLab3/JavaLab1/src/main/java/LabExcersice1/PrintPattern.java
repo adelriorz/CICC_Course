f21fff55855ea0c8bce8c0f7d1e32c7609abf5d2
Armando Del Rio <adelriorz@gmail.com>
2022-12-16 11:16:17 -0800

    Adding previous classes to compile them

---
+++ Java-OOP/JavaLab3/JavaLab1/src/main/java/LabExcersice1/PrintPattern.java
@@ -0,0 +1,18 @@
+package LabExcersice1;
+
+/**
+ * @author Armando Del Rio
+ */
+public class PrintPattern {
+    
+    public static void displayJavaHarcoded(){
+        System.out.println("   J    a   v     v  a\n" +
+        "   J   a a   v   v  a a\n" +
+        "J  J  aaaaa   V V  aaaaa\n" +
+        " JJ  a     a   V  a     a");
+    }
+    
+    public static void main(String[] args) {
+        displayJavaHarcoded();
+    }
+}
